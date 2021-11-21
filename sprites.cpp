3bb60e99045942dfd3fc02e5713398ffc115cc3b
lilliaaw <lilliaaw@stud.ntnu.no>
2021-11-21 14:27:26 +0100

    added pellet model

--- sprites.cpp
+++ sprites.cpp
@@ -166,7 +166,7 @@ GLuint Ghosts::LoadModel(const std::string path)
 	std::string err;
 
 	//We use tinobj to load our models. Feel free to find other .obj files and see if you can load them.
-	tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, (path + "/snowman.obj").c_str(), path.c_str());
+	tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, (path + "/monster.obj").c_str(), path.c_str());
 
 	if (!warn.empty()) {
 		std::cout << warn << std::endl;
